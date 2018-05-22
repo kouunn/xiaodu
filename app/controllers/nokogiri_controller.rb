6257a533c790f122716f13a941fde2b713c74bb5
Vagrant <gao_yun@qq.com>
2018-05-22 16:23:27 +0000

    version 1.0

--- app/controllers/nokogiri_controller.rb
+++ app/controllers/nokogiri_controller.rb
@@ -6,26 +6,22 @@ class NokogiriController < ApplicationController
   def index
   	@key_word = params[:search]
 
-  	baidu_url = URI.escape("http://www.baidu.com/s?wd=#{@key_word}")#URI.escape("http://www.baidu.com/s?wd=#{@key_word}")
-  	doc= Nokogiri::HTML(open(baidu_url))
-  	decode_url = URI.escape("http://www.baidu.com/link?url=5FEJyFnthNeUOZD98UrIzN7O0qZWtc5UknDg5Fk4OrJbongkbcQsLm8Q94reMM65UPqHhXevCv5tSDiWKXXzpa&wd=&eqid=c920619900032cf5000000065adc6bb6")
-  	
+    baidu_url = URI.escape("http://www.baidu.com/s?wd=#{@key_word}")
+    doc= Nokogiri::HTML(open(baidu_url))
     @result=doc
 
-  	
-    @results = []#Hash.new
-    doc.css('.result h3 a').each_with_index do |item,index|
+    @results = []
+    doc.css('.t').each_with_index do |item,index|
       url=URI.extract(item.to_html).first
 
       @results[index] = {
-        :title => item.at_css(".t"),
-        :url => url,
-        :true_url => get_real_url(url)
+        :title => item.at_css("a").content,
+        :true_url => get_real_url(item.at_css("a")[:href])
       }
     end
 
-  	baidu_url = URI.escape('https://www.baidu.com/s?wd=#{@key_word}')
-  	@result = Nokogiri::HTML(open(baidu_url))
+  	#baidu_url = URI.escape('https://www.baidu.com/s?wd=#{@key_word}')
+  	#@result = Nokogiri::HTML(open(baidu_url))
   	
 
   end
@@ -38,9 +34,12 @@ class NokogiriController < ApplicationController
     #input_url = URI.escape(url)
     url+="&wd=&eqid=c88708b700042854000000065adc78de"
     response = open(url,:allow_redirections => :safe).read
-    tmp_url =URI.extract(response)
+    tmp_url =URI.extract(response, ["http", "https"])
     tmp_url.first
+  rescue 
+    nil
   end
+
 end
 
 
